759e3d56bcb7b006aedc28cd7a06edce53f38ebc
poppy <poppy@pavi>
2022-09-12 12:47:09 +0530

    commit and push by user1

---
+++ src/test/java/organization/CreateOrgTest.java
@@ -0,0 +1,13 @@
+package organization;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("org created");
+		
+	}
+
+}
---
+++ src/test/java/practice/DemoTest.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void demo()
+	{
+		System.out.println("this is sample");
+	}
+
+}
